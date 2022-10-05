f03cfb5ddc4af1ae99fff6e9dcfc65c3ca6608c2
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-05 18:45:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
+}
